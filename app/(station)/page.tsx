8f228466a1bd868bba4f93f52786810341f00601
The Wizard of Hahz <info@luvnft.com>
2024-08-07 08:59:54 -0400

    updated

--- app/(station)/page.tsx
+++ app/(station)/page.tsx
@@ -1,6 +1,7 @@
 import ApplySection from "./components/apply";
 import CommunitySection from "./components/community";
 import HeroSection from "./components/hero";
+import { metadata } from 'next-sanity/studio/metadata';
 import PartnersSection from "./components/partners";
 
 export default function Home() {
